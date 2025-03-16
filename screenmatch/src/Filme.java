336a479d929c5405049f506ce0d181914b4f7258
Wilson Gomes <69987793+WilsonGomes95@users.noreply.github.com>
2025-03-16 00:12:38 +0000

    Atualizando as classes

--- screenmatch/src/Filme.java
+++ screenmatch/src/Filme.java
@@ -1,23 +1,73 @@
 public class Filme {
-    String nome;
-    int anoDeLancamento;
-    boolean incluidoNoPlano;
-    double SomaDasAvaliacoes;
-    int totalDeAvaliacoes;
-    int duracaoEmMinutos;
+    private String nome;
+    private int anoDeLancamento;
+    private boolean incluidoNoPlano = false;
+    private double SomaDasAvaliacoes = 0;
+    private int totalDeAvaliacoes = 0;
+    private int duracaoEmMinutos;
+
+
+    public Filme(){
+    }
+
+    public Filme(String nome, int anoDeLancamento, int duracaoEmMinutos){
+        this.nome = nome;
+        this.anoDeLancamento = anoDeLancamento;
+        this.duracaoEmMinutos = duracaoEmMinutos;
+
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getAnoDeLancamento() {
+        return anoDeLancamento;
+    }
+
+    public int getDuracaoEmMinutos() {
+        return duracaoEmMinutos;
+    }
+
+    public int getTotalDeAvaliacoes() {
+        return totalDeAvaliacoes;
+    }
+
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setAnoDeLancamento(int anoDeLancamento) {
+        this.anoDeLancamento = anoDeLancamento;
+    }
+
+    public void setIncluidoNoPlano(boolean incluidoNoPlano) {
+        this.incluidoNoPlano = incluidoNoPlano;
+    }
+
+    public void setDuracaoEmMinutos(int duracaoEmMinutos) {
+        this.duracaoEmMinutos = duracaoEmMinutos;
+    }
+
+
+
 
 
     @Override
     public String toString(){
-        return String.format("""
-        Nome: %s
-        Ano de lançamento: %d
-        Avaliação: %.1f
-        """, nome, anoDeLancamento, SomaDasAvaliacoes);
+        return String.format("Nome: %s\nAno de lançamento: %d\nAvaliação: %.1f\n", nome, anoDeLancamento, SomaDasAvaliacoes);
     }
 
     public void avaliacao(double nota){
         SomaDasAvaliacoes += nota;
+        totalDeAvaliacoes++;
     }
+
+    public double pegaMedia(){
+        return SomaDasAvaliacoes / totalDeAvaliacoes;
+    }
+
+    
 
 }
--- screenmatch/src/Principal.java
+++ screenmatch/src/Principal.java
@@ -1,11 +1,22 @@
 public class Principal {
     public static void main(String[] args) throws Exception {
-        Filme meuFilme = new Filme();
-        meuFilme.nome = "O Poderoso Chefão";
-        meuFilme.anoDeLancamento = 1972;
-        meuFilme.duracaoEmMinutos = 175;
+        Filme meuFilme = new Filme("O Poderoso Chefão", 1972, 175);
 
         System.out.println(meuFilme);
+        meuFilme.avaliacao(5);
+        meuFilme.avaliacao(4.5);
+        meuFilme.avaliacao(4.5);
+        System.out.println(meuFilme);
+        System.out.println("Média: " + String.format("%.2f",meuFilme.pegaMedia()));
+        
+
+
+
+
+
+
+
+
 
     }
 }
